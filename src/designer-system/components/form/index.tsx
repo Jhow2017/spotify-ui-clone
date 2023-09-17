a67a4845d0654f5a3273a8d51ab37158d12aeee3
Jhow Gama <jhongama_jesus@hotmail.com>
2023-09-17 03:14:22 -0400

    create DsInput

---
+++ src/designer-system/components/form/index.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import {
+    TextInput,
+    TextInputProps,
+    KeyboardTypeOptions,
+    ViewStyle,
+    TextStyle,
+} from "react-native";
+
+type InputTypes = "text" | "search" | "date" | "password" | "number";
+
+type DsInputProps = Omit<TextInputProps, "textAlign"> &
+    TextStyle & {
+        type: InputTypes;
+    };
+
+const transformPlaceholder = (
+    text: string | undefined,
+    transform: TextStyle["textTransform"]
+) => {
+    if (!text) return text;
+
+    switch (transform) {
+        case "capitalize":
+            return text.toLowerCase().replace(/\b\w/g, (c) => c.toUpperCase());
+        case "uppercase":
+            return text.toUpperCase();
+        case "lowercase":
+            return text.toLowerCase();
+        default:
+            return text;
+    }
+};
+
+const DsInput: React.FC<DsInputProps> = ({ type, ...props }) => {
+    const { children, textTransform, placeholder, ...attr } = props;
+
+    let keyboardType: KeyboardTypeOptions = "default";
+
+    if (type === "number") {
+        keyboardType = "numeric";
+    } else if (type === "search") {
+        keyboardType = "web-search";
+    } else if (type === "date") {
+        keyboardType = "numeric";
+    } else if (type === "password") {
+        // Use appropriate keyboardType for password if needed
+    }
+
+    const [transformedPlaceholder, setTransformedPlaceholder] = useState<
+        string | undefined
+    >(placeholder);
+
+    useEffect(() => {
+        if (placeholder && textTransform) {
+            setTransformedPlaceholder(
+                transformPlaceholder(placeholder, textTransform)
+            );
+        } else {
+            setTransformedPlaceholder(placeholder);
+        }
+    }, [placeholder, textTransform]);
+
+    return (
+        <TextInput
+            {...(attr as TextInputProps)}
+            placeholderTextColor={attr.placeholderTextColor || "#A7A7A7"}
+            keyboardType={keyboardType}
+            placeholder={transformedPlaceholder}
+            style={{
+                width: attr?.width ?? 334,
+                height: attr?.height ?? 80,
+                flexShrink: 0,
+                borderRadius: attr?.borderRadius ?? 30,
+                borderWidth: attr?.borderWidth ?? 1,
+                borderColor: attr?.borderColor ?? "#fff",
+                opacity: attr?.opacity ?? 0.14,
+                color: attr?.color ?? "#fff",
+                fontSize: attr?.fontSize ?? 16,
+                fontStyle: attr?.fontStyle ?? "normal",
+                fontWeight: attr?.fontWeight ?? "700",
+                padding: attr?.padding ?? 30,
+                ...(attr.style as ViewStyle),
+            }}
+        />
+    );
+};
+
+export default DsInput;
