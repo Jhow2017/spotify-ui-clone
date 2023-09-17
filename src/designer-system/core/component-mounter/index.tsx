6590886612fcd0dd9f20a709d7ac51514ecd163b
Jhow Gama <jhongama_jesus@hotmail.com>
2023-09-17 17:08:48 -0400

    remove validation case as to ScrollView in ComponentMounter

--- src/designer-system/core/component-mounter/index.tsx
+++ src/designer-system/core/component-mounter/index.tsx
@@ -1,26 +1,20 @@
-import React, { ReactNode } from 'react';
-import { View, ViewProps, ScrollView, ScrollViewProps, ViewStyle } from 'react-native';
+import React, { ReactNode } from "react";
+import { View, ViewProps, ScrollViewProps, ViewStyle } from "react-native";
 
-export interface ComponentMounterType extends ViewProps, ViewStyle, ScrollViewProps {
+export interface ComponentMounterType
+    extends ViewProps,
+        ViewStyle,
+        ScrollViewProps {
     children?: ReactNode;
-    as?: 'View' | 'ScrollView';
-};
+}
 
 const ComponentMounter: React.FC<ComponentMounterType> = (props) => {
-    const { children, as = 'View', ...attr } = props;
+    const { children, ...attr } = props;
 
     return (
-        <>
-            {as === 'View' ? (
-                <View {...attr} style={attr.style}>
-                    {children}
-                </View>
-            ) : (
-                <ScrollView {...attr} style={attr.style}>
-                    {children}
-                </ScrollView>
-            )}
-        </>
+        <View {...attr} style={attr.style}>
+            {children}
+        </View>
     );
 };
 
