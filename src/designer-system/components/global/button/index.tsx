41072b13a9912ba82c95a1915a245a393ed83030
Jhow Gama <jhongama_jesus@hotmail.com>
2023-09-17 19:29:29 -0400

    clean code DsButton

--- src/designer-system/components/global/button/index.tsx
+++ src/designer-system/components/global/button/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import { TouchableOpacity, TouchableOpacityProps, ViewStyle, ActivityIndicator, TextStyle } from 'react-native';
+import React from "react";
+import {
+    TouchableOpacity,
+    TouchableOpacityProps,
+    ViewStyle,
+    ActivityIndicator,
+    TextStyle,
+} from "react-native";
 
 //components
-import { DsText } from '@ds/components/typography';
-import DsIcon, { IconsType } from '@ds/components/global/icon';
+import { DsText } from "@ds/components/typography";
+import DsIcon, { IconsType } from "@ds/components/global/icon";
+import { filterProps } from "src/services/utils/filterProps";
 
 interface DsButtonProps extends TouchableOpacityProps, TextStyle, ViewStyle {
     children: React.ReactNode;
-    variant: 'default' | 'primary' | 'secondary';
-    size: 'small' | 'medium' | 'large';
+    variant: "default" | "primary" | "secondary";
+    size: "small" | "medium" | "large";
     loading?: boolean;
     icon?: IconsType;
     iconLeft?: boolean;
@@ -19,13 +26,32 @@ const DsButton: React.FC<DsButtonProps> = (props) => {
 
     const buttonStyles: ViewStyle = {
         borderRadius: 30,
-        padding: size === 'small' && 4 || size === 'medium' && 12 || size === 'large' && 30 || 0,
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: size === 'small' && 120 || size === 'medium' && 220 || size === 'large' && 329 || 'auto',
-        height: size === 'small' && 40 || size === 'medium' && 52 || size === 'large' && 92 || 'auto',
-        backgroundColor: variant === 'default' ? 'transparent' : variant === 'primary' ? 'blue' : variant === 'secondary' ? '#42C83C' : attr?.backgroundColor,
-        ...attr as ViewStyle,
+        padding:
+            (size === "small" && 4) ||
+            (size === "medium" && 12) ||
+            (size === "large" && 30) ||
+            0,
+        alignItems: "center",
+        justifyContent: "center",
+        width:
+            (size === "small" && 120) ||
+            (size === "medium" && 220) ||
+            (size === "large" && 329) ||
+            "auto",
+        height:
+            (size === "small" && 40) ||
+            (size === "medium" && 52) ||
+            (size === "large" && 92) ||
+            "auto",
+        backgroundColor:
+            variant === "default"
+                ? "transparent"
+                : variant === "primary"
+                ? "blue"
+                : variant === "secondary"
+                ? "#42C83C"
+                : attr?.backgroundColor,
+        ...(attr as ViewStyle),
     };
 
     const textSize = {
@@ -34,45 +60,17 @@ const DsButton: React.FC<DsButtonProps> = (props) => {
         large: 22,
     };
 
-
-    const filterProps = (attr: any): TextStyle => {
-        const allowedProps: (keyof TextStyle)[] = [
-            'color',
-            'fontFamily',
-            'fontSize',
-            'fontStyle',
-            'fontWeight',
-            'letterSpacing',
-            'lineHeight',
-            'textAlign',
-            'textDecorationLine',
-            'textDecorationStyle',
-            'textDecorationColor',
-            'textShadowColor',
-            'textShadowOffset',
-            'textShadowRadius',
-            'textTransform',
-            'testID',
-        ];
-
-        const filteredProps: TextStyle = {};
-
-        for (const prop of allowedProps) {
-            if (attr && attr[prop] !== undefined) {
-                filteredProps[prop] = attr[prop];
-            }
-        }
-
-        return filteredProps;
-    };
-
-
     const textStyles: TextStyle = {
         fontSize: attr.fontSize ?? textSize[size],
-        color: attr.color ?? (variant === 'primary' ? 'red' : (variant === 'secondary' ? '#F6F6F6' : '#fff')),
+        color:
+            attr.color ??
+            (variant === "primary"
+                ? "red"
+                : variant === "secondary"
+                ? "#F6F6F6"
+                : "#fff"),
         ...filterProps(attr),
     };
-
 
     return (
         <TouchableOpacity style={buttonStyles} disabled={loading} {...attr}>
@@ -81,7 +79,7 @@ const DsButton: React.FC<DsButtonProps> = (props) => {
             ) : (
                 <>
                     {iconLeft && icon && <DsIcon icon={icon} />}
-                    <DsText {...textStyles as TextStyle}>{children}</DsText>
+                    <DsText {...(textStyles as TextStyle)}>{children}</DsText>
                     {!iconLeft && icon && <DsIcon icon={icon} />}
                 </>
             )}
