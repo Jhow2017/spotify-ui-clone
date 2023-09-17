f7667920d4777b46d60f12d047f5914d4553431e
Jhow Gama <jhongama_jesus@hotmail.com>
2023-09-17 17:08:06 -0400

    add ScrollViewin SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -12,6 +12,8 @@ import DsInput from "@ds/components/form/input";
 //components
 import Header from "src/components/header";
 import LayoutPublic from "src/components/layout/layout-public";
+import DsDivider from "@ds/components/global/divider";
+import { ScrollView } from "react-native";
 
 const SignInTypesSchema = z.object({
     username: z.string().min(3),
@@ -38,87 +40,122 @@ const SignIn = () => {
     };
 
     return (
-        <LayoutPublic>
-            <Header />
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <DsFlex marginTop={80} flexDirection="column">
-                    <DsText
-                        color="#F2F2F2"
-                        fontSize={30}
-                        lineHeight={35}
-                        fontWeight="700"
-                        textAlign="center"
-                        textTransform="capitalize"
-                    >
-                        Sign in
-                    </DsText>
+        <ScrollView
+            scrollEnabled
+            contentContainerStyle={{
+                flexGrow: 1,
+            }}
+        >
+            <LayoutPublic>
+                <Header />
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <DsFlex marginTop={80} flexDirection="column">
+                        <DsText
+                            color="#F2F2F2"
+                            fontSize={30}
+                            lineHeight={35}
+                            fontWeight="700"
+                            textAlign="center"
+                            textTransform="capitalize"
+                        >
+                            Sign in
+                        </DsText>
 
-                    <DsFlex flexDirection="column" gap={16} marginTop={38}>
-                        <Controller
-                            control={control}
-                            name="username"
-                            render={({ field }) => (
-                                <DsInput
-                                    type="text"
-                                    size="medium"
-                                    id="username"
-                                    onChangeText={field.onChange}
-                                    value={field.value}
-                                    placeholder="Enter username or email"
-                                    textTransform="capitalize"
-                                    error={
-                                        errors.username &&
-                                        errors.username.message
-                                    }
-                                />
-                            )}
-                        />
+                        <DsFlex flexDirection="column" gap={16} marginTop={38}>
+                            <Controller
+                                control={control}
+                                name="username"
+                                render={({ field }) => (
+                                    <DsInput
+                                        type="text"
+                                        size="medium"
+                                        id="username"
+                                        onChangeText={field.onChange}
+                                        value={field.value}
+                                        placeholder="Enter username or email"
+                                        textTransform="capitalize"
+                                        error={
+                                            errors.username &&
+                                            errors.username.message
+                                        }
+                                    />
+                                )}
+                            />
 
-                        <Controller
-                            control={control}
-                            name="password"
-                            render={({ field }) => (
-                                <DsInput
-                                    size="medium"
-                                    id="password"
-                                    onChangeText={field.onChange}
-                                    value={field.value}
-                                    type="password"
-                                    placeholder="Password"
-                                    error={
-                                        errors.password &&
-                                        errors.password.message
-                                    }
-                                />
-                            )}
-                        />
+                            <Controller
+                                control={control}
+                                name="password"
+                                render={({ field }) => (
+                                    <DsInput
+                                        size="medium"
+                                        id="password"
+                                        onChangeText={field.onChange}
+                                        value={field.value}
+                                        type="password"
+                                        placeholder="Password"
+                                        error={
+                                            errors.password &&
+                                            errors.password.message
+                                        }
+                                    />
+                                )}
+                            />
+                        </DsFlex>
+
+                        <DsText
+                            color="#A0A0A0"
+                            fontSize={17}
+                            lineHeight={25}
+                            fontWeight="400"
+                            textAlign="left"
+                            marginTop={21}
+                        >
+                            Recovery password
+                        </DsText>
+                        <DsButton
+                            variant="secondary"
+                            size="large"
+                            paddingHorizontal={36}
+                            fontWeight="700"
+                            lineHeight={22}
+                            borderRadius={30}
+                            marginTop={22}
+                            onPress={handleSubmit(onSubmit)}
+                        >
+                            Sign In
+                        </DsButton>
                     </DsFlex>
-
-                    <DsText
-                        color="#A0A0A0"
-                        fontSize={17}
-                        lineHeight={25}
-                        fontWeight="400"
-                        textAlign="left"
-                        marginTop={21}
-                    >
-                        Recovery password
+                </TouchableWithoutFeedback>
+                <DsFlex
+                    marginTop={31}
+                    alignItems="center"
+                    justifyContent="center"
+                    gap={8}
+                >
+                    <DsDivider
+                        gradient={{
+                            colors: ["#252525", "#5B5B5B"],
+                            start: [1, 0],
+                            end: [0, 0],
+                        }}
+                        width={150}
+                        height={1}
+                    />
+                    <DsText fontSize={14} color="#DCDCDC" bottom={2}>
+                        Or
                     </DsText>
-                    <DsButton
-                        variant="secondary"
-                        size="large"
-                        paddingHorizontal={36}
-                        fontWeight="700"
-                        lineHeight={22}
-                        borderRadius={30}
-                        marginTop={22}
-                        onPress={handleSubmit(onSubmit)}
-                    >
-                        Sign In
-                    </DsButton>
+                    <DsDivider
+                        gradient={{
+                            colors: ["#252525", "#5B5B5B"],
+                            start: [0, 0],
+                            end: [1, 0],
+                        }}
+                        width={150}
+                        height={1}
+                    />
                 </DsFlex>
-            </TouchableWithoutFeedback>
-        </LayoutPublic>
+            </LayoutPublic>
+        </ScrollView>
     );
 };
 
