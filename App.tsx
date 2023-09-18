24ec56ff255ac14195f1b1286620626ecbc8911d
Jhow Gama <jhongama_jesus@hotmail.com>
2023-09-18 18:41:29 -0400

    fonts add/remove

--- App.tsx
+++ App.tsx
@@ -1,8 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import Navigation from "src/stacks/navigation";
+import {
+    useFonts,
+    Inter_900Black,
+    Inter_400Regular,
+    Inter_700Bold,
+    Inter_300Light,
+} from "@expo-google-fonts/inter";
+
 import "react-native-gesture-handler";
+import Loading from "@components/loading";
 
 export default function App() {
+    let [fontsLoaded] = useFonts({
+        Inter_900Black,
+        Inter_400Regular,
+        Inter_700Bold,
+        Inter_300Light,
+    });
+
+    if (!fontsLoaded) {
+        return <Loading />;
+    }
+
     return (
         <>
             <StatusBar style="light" translucent />
