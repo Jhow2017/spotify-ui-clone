f2e651818a1b97b0a03d90194776ba881ec916b6
Jhow Gama <jhongama_jesus@hotmail.com>
2023-09-18 15:39:30 -0400

    add "gap" native of DsFlex

--- src/designer-system/layout/flex/index.tsx
+++ src/designer-system/layout/flex/index.tsx
@@ -1,41 +1,20 @@
 import React from "react";
-import { View, ViewStyle, FlexStyle } from "react-native";
+import { FlexStyle } from "react-native";
 import ComponentMounter, {
     ComponentMounterType,
 } from "@ds/core/component-mounter";
 
-export interface DsFlexType extends FlexStyle, ComponentMounterType {
-    gap?: number;
-}
+export interface DsFlexType extends FlexStyle, ComponentMounterType {}
 
 const DsFlex: React.FC<DsFlexType> = (props) => {
-    const { children, gap, ...attr } = props;
-
-    const modifiedChildren = React.Children.map(children, (child, index) => {
-        if (index === 0) {
-            return child;
-        }
-
-        const marginStyle: ViewStyle = {};
-        if (gap) {
-            if (attr?.flexDirection === "column") {
-                marginStyle.marginTop = gap;
-            } else {
-                marginStyle.marginLeft = gap;
-            }
-        }
-
-        return (
-            <>
-                <View style={marginStyle} />
-                {child}
-            </>
-        );
-    });
+    const { children, ...attr } = props;
 
     return (
-        <ComponentMounter style={{ flexDirection: "row", ...attr }} {...attr}>
-            {modifiedChildren}
+        <ComponentMounter
+            style={[attr?.style, { flexDirection: "row" }]}
+            {...attr}
+        >
+            {children}
         </ComponentMounter>
     );
 };
