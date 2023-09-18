97c5e3934605f18cf89332bc6b8d519faa124c73
Jhow Gama <jhongama_jesus@hotmail.com>
2023-09-18 18:40:49 -0400

    add new text HomeScreen

--- src/screens/dashboard/Home/index.tsx
+++ src/screens/dashboard/Home/index.tsx
@@ -1,12 +1,12 @@
 import { ScrollView } from "react-native";
 
 //@Ds
-import { DsFlex } from "@ds/layout";
+import { DsBox, DsFlex } from "@ds/layout";
+import { DsText } from "@ds/components/typography";
 
 //components
-import Header from "src/components/header";
-import LayoutPublic from "src/components/layout/layout-public";
-import { DsText } from "@ds/components/typography";
+import Header from "@components/header";
+import LayoutPublic from "@components/layout/layout-public";
 
 const HomeScreen = () => {
     return (
@@ -18,17 +18,47 @@ const HomeScreen = () => {
         >
             <LayoutPublic>
                 <Header marginTop={30} />
-                <DsFlex marginTop={80} flexDirection="column">
-                    <DsText
-                        color="#F2F2F2"
-                        fontSize={20}
-                        lineHeight={35}
-                        fontWeight="700"
-                        textTransform="capitalize"
-                    >
-                        Home
-                    </DsText>
-                </DsFlex>
+                <DsBox
+                    backgroundColor={"#42C83C"}
+                    width={"100%"}
+                    height={118}
+                    borderRadius={30}
+                    marginTop={65}
+                    position="relative"
+                    overflow="hidden"
+                    padding={32}
+                >
+                    <DsFlex flexDirection="column">
+                        <DsText
+                            fontFamily={"Inter_300Light"}
+                            fontWeight={"700"}
+                            fontSize={14}
+                            lineHeight={14}
+                            textTransform={"capitalize"}
+                            color={"#FBFBFB"}
+                        >
+                            New album
+                        </DsText>
+                        <DsText
+                            color="#FBFBFB"
+                            fontFamily="Inter_900Black"
+                            fontSize={19}
+                            fontWeight="700"
+                            textTransform="capitalize"
+                        >
+                            Happier Than Ever
+                        </DsText>
+                        <DsText
+                            color="#F2F2F2"
+                            fontSize={20}
+                            lineHeight={35}
+                            fontWeight="700"
+                            textTransform="capitalize"
+                        >
+                            Billie Eilish
+                        </DsText>
+                    </DsFlex>
+                </DsBox>
             </LayoutPublic>
         </ScrollView>
     );
